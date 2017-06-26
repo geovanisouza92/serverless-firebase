ee3795346ba127f33a56f9e94974c9ef43d0e610
Geovani de Souza <geovani@serrabits.com.br>
2017-06-26 18:23:12 -0300

    CHANGES

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,28 @@
 const functions = require('firebase-functions')
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-
+const url = require('url')
 const next = require('next')
 
-exports.next = functions.https.onRequest((req, res) => {
+var app, handle
+
+if (!/\/tmp/.test(__dirname)) {
   // NOTE: Dev mode doesn't work like a charm :/
   // (Same problem reported at https://github.com/zeit/next.js/issues/2123)
-  const app = next({ dev: false, conf: { distDir: 'next' } })
-  const handle = app.getRequestHandler()
+  // NOTE: Probably this should be a configuration
+  const dev = /* process.env.NODE_ENV !== 'production' */ false
+  const conf = {
+    // NOTE: Probably this should be a configuration
+    assetPrefix: '/serverless-firebase/us-central1/next',
+    distDir: 'next'
+  }
 
-  return app.prepare().then(() => handle(req, res))
+  // FIXME: Firebase deploy command tries to load the code, causing an error,
+  // so, we delay app initialization
+  app = next({ dev, conf })
+  handle = app.getRequestHandler()
+}
+
+exports.next = functions.https.onRequest((req, res) => {
+  return app.prepare().then(() =>
+    handle(req, res, url.parse(req.url, true))
+  )
 })
